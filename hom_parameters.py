a241d800199f2fbac34d861745ffee9b11d958fb
Tadeáš Fryčák <tadeas@frycak.com>
2025-03-05 22:00:11 +0100

    update: default modele parameters

--- hom_parameters.py
+++ hom_parameters.py
@@ -3,7 +3,7 @@ import os
 
 # === PARAMETERS ===
 N_PEOPLE = 1_000_000    # number of individuals in the model
-N_DAYS = 2 * 52 * 7     # number of simulation days (approximately 2 years)
+N_DAYS = 100 * 7     # number of simulation days (approximately 2 years)
 N_RUNS = 100             # number of simulations
 
 # Death
@@ -12,12 +12,12 @@ DEATH_DISTRIBUTION = lambda: random.randint(0, N_DAYS)
 
 # Vaccination
 VACCINATION_P = 0.8     # number of vaccinated (=> N_PEOPLE * P_VACCINATED = approximate number of vaccinations)
-VACCINATION_MEAN = 6*4  # mean of the vaccination week (in weeks)
-VACCINATION_STD = 1*4   # standard deviation of the vaccination week (in weeks)
+VACCINATION_MEAN = 6*4*7  # mean of the vaccination week (in weeks)
+VACCINATION_STD = 1*4*7   # standard deviation of the vaccination week (in weeks)
 VACCINATION_DISTRIBUTION = lambda: max(0, min(N_DAYS, round(random.gauss(VACCINATION_MEAN, VACCINATION_STD))))
 
 # Healthy Vaccine Effect (HVE)
-HVE_K = 3    # How many weeks to look into the future to see if the subject will die
+HVE_K = 20    # How many weeks to look into the future to see if the subject will die
 HVE_P = 0.5  # Strength of HVE; probability that a vaccine will NOT be given due to HVE at vacc_day == death_day
 
 # Other
@@ -27,7 +27,8 @@ YEAR_DAYS = 365.25
 MIN_DEATH_COUNT = 1     # minimum number of deaths per day to be included in the plot
 RESULT_DIR = "result"
 RESULT_FILE = os.path.join(RESULT_DIR, f"{HVE_P=}_{HVE_K=}_{N_RUNS=}")
-
+FIG_SIZE = (11, 4)
+# FIG_SIZE = (6, 4)
 # === SANITY CHECKS ===
 assert 0 < HVE_K < N_DAYS, "HVE_K must be smaller than N_days and greater than 0"
 assert 0 <= HVE_P <= 1, "Probability of HVE must be between 0 and 1"
@@ -35,10 +36,5 @@ assert 0 < DEATH_P < 1, "Probability of death must be between 0 and 1"
 assert 0 < VACCINATION_P < 1, "Probability of vaccination must be between 0 and 1"
 assert N_DAYS > 0, f"Number of days must be positive"
 assert N_PEOPLE > 0, f"Number of people must be positive"
-
-# Convert weeks to days
-HVE_K *= 7
-VACCINATION_MEAN *= 7
-VACCINATION_STD *= 7
 
 os.makedirs(RESULT_DIR, exist_ok=True)
--- hom_visualization.py
+++ hom_visualization.py
@@ -15,7 +15,7 @@ def plot_acm(x: list[int], runs_all: list[float], runs_vacc: list[float], runs_n
     :param runs_norm: runs with shape of (N_RUNS, ) for unvaccinated individuals
     :return: None
     """
-    plt.figure(figsize=(11, 4))
+    plt.figure(figsize=FIG_SIZE)
     plt.plot(x, np.mean(runs_vacc, axis=0), label="Mean: vaccinated", color="r")
     plt.plot(x, np.mean(runs_norm, axis=0), label="Mean: unvaccinated", color="b")
     plt.plot(x, np.mean(runs_all, axis=0), label="Mean: all", color="k")
@@ -30,7 +30,7 @@ def plot_acm(x: list[int], runs_all: list[float], runs_vacc: list[float], runs_n
     plt.ylabel("ACM [deaths per person-year]")
     plt.xlabel("Week number")
     plt.ylim(bottom=0)
-    plt.legend(loc="lower right")
+    plt.legend(loc="upper right")
     plt.tight_layout()
     plt.savefig(f"{RESULT_FILE}_acm.svg")
     plt.clf()
@@ -43,7 +43,7 @@ def plot_vaccination_death_difference(runs_hist_x: list[np.ndarray[int]], runs_h
     :param runs_hist_y: runs with shape of (N_RUNS, ) for y of the histogram
     :return: None
     """
-    plt.figure(figsize=(11, 4))
+    plt.figure(figsize=FIG_SIZE)
     big_histogram_x = list(range(min(min(i) for i in runs_hist_x), max(max(i) for i in runs_hist_x) + 1))
     big_histogram_y = np.zeros((N_RUNS, len(big_histogram_x)))
 
@@ -52,15 +52,15 @@ def plot_vaccination_death_difference(runs_hist_x: list[np.ndarray[int]], runs_h
             if v in hx:
                 big_histogram_y[n, v] = hy[hx == v]
 
-    big_histogram_x = big_histogram_x[:len(big_histogram_y[0]) // 2]
-    big_histogram_y = big_histogram_y[:, :len(big_histogram_y[0]) // 2]
+    big_histogram_x = big_histogram_x[:HVE_K * 5]
+    big_histogram_y = big_histogram_y[:, :HVE_K * 5]
     plt.plot(big_histogram_x, np.mean(big_histogram_y, axis=0), label="Mean", color="b")
     plt.fill_between(big_histogram_x,
                      np.mean(big_histogram_y, axis=0) - np.std(big_histogram_y, axis=0),
                      np.mean(big_histogram_y, axis=0) + np.std(big_histogram_y, axis=0), color="b", alpha=0.2,
                      label="Mean ± std")
     plt.ylabel("Number of deaths on that day")
-    plt.xlabel("Number of weeks since vaccination")
+    plt.xlabel("Number of days since vaccination")
     plt.legend()
     plt.tight_layout()
     plt.savefig(f"{RESULT_FILE}_hist.svg")
@@ -74,7 +74,7 @@ def plot_vaccine_effectiveness(x, runs_effectiveness):
     :param runs_effectiveness: runs with shape of (N_RUNS, ) for vaccine effectiveness
     :return: None
     """
-    plt.figure(figsize=(11, 4))
+    plt.figure(figsize=FIG_SIZE)
     plt.plot(x, np.mean(runs_effectiveness, axis=0), label="Mean", color="g")
     plt.fill_between(x,
                      np.mean(runs_effectiveness, axis=0) - np.std(runs_effectiveness, axis=0),
